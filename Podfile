b5dbc240811c240fbb5dffda62647c68013a0259
Joel Davis <joeld42@gmail.com>
2018-01-02 17:42:38 -0500

    Updated to swift4, added SnapKit and Chameleon

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Subway Intervals' do
+    pod 'SnapKit', '4.0.0'
+    pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+end
--- Subway Intervals/SettingsViewController.swift
+++ Subway Intervals/SettingsViewController.swift
@@ -7,7 +7,34 @@
 //
 
 import UIKit
+import SnapKit
+import ChameleonFramework
 
 class SettingsViewController: UIViewController
 {
+    lazy var presetsLabel = UILabel()
+    
+//     lazy var presetsCollection : UICollectionView = UICollectionView()
+    
+    override func viewDidLoad()
+    {
+        presetsLabel.text = "Presets"
+        //presetsLabel.textColor = UIColor.flatYellow
+        presetsLabel.textColor = UIColor.flatYellow
+        self.view.addSubview( presetsLabel )
+        
+        self.view.setNeedsUpdateConstraints();
+    }
+    
+    override func updateViewConstraints()
+    {
+        presetsLabel.snp.remakeConstraints{ (make) -> Void in
+            make.top.equalTo( view.safeAreaLayoutGuide.snp.top )
+            make.left.equalTo( view )
+            make.right.equalTo( view )
+            make.height.equalTo( 44.0 )
+        }
+        
+        super.updateViewConstraints()
+    }
 }
